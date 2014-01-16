4aeb8fbc64dc3b043f2558bcab7241dd3a5276f2
Zhu Li <zhuli19901106@gmail.com>
2014-01-16 22:55:47 +0800

    add leetcode files
    201401162255

--- binary-tree-postorder-traversal(AC).cpp
+++ binary-tree-postorder-traversal(AC).cpp
@@ -1,71 +1,25 @@
-// 1CE, 1RE, 1AC, fair well
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
-class TreeNodeSt {
-public:
-    TreeNode *ptr;
-    int count;
-    // 1CE here, forgot to add default value
-    TreeNodeSt(TreeNode *_ptr = nullptr) {
-        ptr = _ptr;
-        count = 0;
-    }
-};
- 
+// 1AC, the trivial recursive version.
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         result.clear();
-        stack.clear();
-        if(root == nullptr){
-            return result;
-        }
-        
-        TreeNodeSt st;
-        TreeNode *tmp;
-        sp = 0;
-        stack.push_back(TreeNodeSt(root));
-        // 1RE here, forgot to add sp by 1
-        ++sp;
-        while(stack.size() > 0){
-            if(stack[sp - 1].count >= 2){
-                result.push_back(stack[sp - 1].ptr->val);
-                stack.pop_back();
-                --sp;
-                continue;
-            }
-            if(stack[sp - 1].count == 0){
-                ++stack[sp - 1].count;
-                if(stack[sp - 1].ptr->left != nullptr){
-                    tmp = stack[sp - 1].ptr;
-                    stack.push_back(TreeNodeSt(tmp->left));
-                    ++sp;
-                }
-            }else{
-                ++stack[sp - 1].count;
-                if(stack[sp - 1].ptr->right != nullptr){
-                    tmp = stack[sp - 1].ptr;
-                    stack.push_back(TreeNodeSt(tmp->right));
-                    ++sp;
-                }
-            }
-        }
-        stack.clear();
-        
+		
+		if(root == nullptr){
+			return result;
+		}
+		
+		preorderTraversalRecursive(root);
+		
         return result;
     }
 private:
-    vector<TreeNodeSt> stack;
+    void preorderTraversalRecursive(TreeNode *root) {
+        if(root == nullptr){
+            return;
+        }
+        preorderTraversalRecursive(root->left);
+        preorderTraversalRecursive(root->right);
+        result.push_back(root->val);
+    }
     vector<int> result;
-    int sp;
 };
--- insertion-sort-list(AC).cpp
+++ insertion-sort-list(AC).cpp
@@ -1,51 +1,38 @@
-// 1CE, 1WA, 1AC, good, but not good enough
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *insertionSortList(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         if(nullptr == head){
             return head;
         }
         ListNode *ptr, *root;
         ListNode *p1, *p2;
         
-        // 1CE here, ListNode(), nt Node()
-        root = new ListNode(INT_MIN);
+        root = head;
+		head = head->next;
+		root->next = nullptr;
         while(head != nullptr){
-            if(root->next == nullptr){
-                ptr = head->next;
-                root->next = head;
-                root->next->next = nullptr;
-                head = ptr;
-            }else{
-                p1 = head->next;
-                ptr = root;
-                while(ptr->next != nullptr){
-                    if(ptr->val <= head->val && ptr->next->val >= head->val){
-                        break;
-                    }else{
-                        ptr = ptr->next;
-                    }
-                }
-                // 1WA here, the case of insertion at tail is ignored 
-                p2 = ptr->next;
-                ptr->next = head;
-                head->next = p2;
-                head = p1;
-            }
+			p1 = head->next;
+			ptr = root;
+			if(head->val <= root->val){
+				head->next = root;
+				root = head;
+			}else{
+				while(ptr->next != nullptr){
+					if(head->val >= ptr->val && head->val <= ptr->next->val){
+						break;
+					}else{
+						ptr = ptr->next;
+					}
+				}
+				p2 = ptr->next;
+				ptr->next = head;
+				head->next = p2;
+			}
+			head = p1;
         }
         
-        head = root->next;
-        delete root;
+		// 1WA here, not head = root->next;
+        head = root;
         return head;
     }
 };
